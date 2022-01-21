e2bfc1b5b3a34dba1cebebf9f5e23d43a81a5e3a
Damien PÉDRON <pedron.damien@outlook.fr>
2022-01-21 13:44:47 +0100

    Some modifications

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/css/index.css';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Housing from './pages/Housing';
+import About from './pages/About';
+import Error from './pages/NotFound';
+
+/**
+ * This is the router of the app
+ */
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Header />
+    <Routes basename="/DamienPedron_11_07012022">
+      <Route exact path="/DamienPedron_11_07012022/" element={<Home />} />
+      <Route
+        path="/DamienPedron_11_07012022/housing-:id"
+        element={<Housing />}
+      />
+      <Route path="/DamienPedron_11_07012022/about" element={<About />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+    <Footer />
   </BrowserRouter>,
   document.getElementById('root')
 );
